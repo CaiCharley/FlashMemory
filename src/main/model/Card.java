eed7d081593dbaf46bbceec896bde90be4ca7809
CaiCharley <charley.cai113@gmail.com>
2020-10-02 02:32:22 -0700

    make and test Card

--- src/main/model/Card.java
+++ src/main/model/Card.java
@@ -2,4 +2,29 @@ package model;
 
 // A flash card with a question and answer. Extends the StudyMaterial class.
 public class Card extends StudyMaterial {
+    private String question;
+    private String answer;
+
+    //effects: makes card with question and answer with whitespaces trimmed
+    public Card(String q, String a) {
+        this.question = q.trim();
+        this.answer = a.trim();
+    }
+
+    //getters and setters
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
 }
---
+++ src/main/tests/tests/TestCard.java
@@ -0,0 +1,29 @@
+package tests;
+
+import model.Card;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCard {
+    Card c1;
+    Card c2;
+
+    @Test
+    void testCard() {
+        c1 = new Card("What is the powerhouse of the cell?", "Mitochondria");
+        c2 = new Card("What is the area of a circle?  ", " A = πr^2");
+
+        assertEquals("Mitochondria", c1.getAnswer());
+        assertEquals("What is the powerhouse of the cell?", c1.getQuestion());
+        assertEquals("A = πr^2", c2.getAnswer());
+        assertEquals("What is the area of a circle?", c2.getQuestion());
+
+        c1.setQuestion("How does a cell divide?");
+        c1.setAnswer("Mitosis");
+
+        assertEquals("How does a cell divide?", c1.getQuestion());
+        assertEquals("Mitosis", c1. getAnswer());
+    }
+
+}
