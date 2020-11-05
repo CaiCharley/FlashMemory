d354433d1486473b331eeabac67709ffbccc459d
CaiCharley <charley.cai113@gmail.com>
2020-11-04 21:06:51 -0800

    add save and save on exit features

--- src/main/ui/gui/FlashMemoryGUI.java
+++ src/main/ui/gui/FlashMemoryGUI.java
@@ -3,10 +3,14 @@ package ui.gui;
 import model.Semester;
 import model.StudyCollection;
 import persistence.JsonReader;
+import persistence.JsonWriter;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Stack;
 
@@ -25,7 +29,7 @@ public class FlashMemoryGUI extends JFrame {
     private JButton changeSemesterButton;
     private JLabel semesterNameLabel;
     private JPanel mainPanel;
-    private JButton button1;
+    private JButton saveSemesterButton;
 
     //effects: makes new FlashMemoryGUI and initializes application fields.
     public FlashMemoryGUI(String title) {
@@ -45,12 +49,50 @@ public class FlashMemoryGUI extends JFrame {
     //effects: initializes JFrame fields and configuration from form
     private void initializeJFrame() {
         changeSemesterButton.addActionListener(e -> initializeSemester());
+        saveSemesterButton.addActionListener(e -> saveSemester());
 
         add(mainPanel);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                terminate();
+            }
+        });
         pack();
         centreOnScreen();
         setVisible(true);
+    }
+
+    private void terminate() {
+        Object[] options = {"Save", "Don't Save"};
+        int n = JOptionPane.showOptionDialog(this,
+                "Would you like to save any changes before quitting?",
+                "Save Changes",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[0]);
+
+        if (n == 0) {
+            saveSemester();
+        }
+
+        System.exit(0);
+    }
+
+    //effects: saves semester to file
+    // adapted from JsonSerializationDemo @ https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo.git
+    private void saveSemester() {
+        String filePath = JSON_DIRECTORY + semester.getName() + ".json";
+        JsonWriter writer = new JsonWriter(filePath);
+        try {
+            writer.open();
+            writer.write(semester);
+            writer.close();
+            JOptionPane.showMessageDialog(this, "Saved " + semester.getName());
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(this, "Unable to write to file:\n" + filePath);
+        }
     }
 
     //modifies: this
@@ -94,7 +136,7 @@ public class FlashMemoryGUI extends JFrame {
                 null,
                 "My Semester");
         if (semesterName != null) {
-            semester = new Semester(semesterName);
+            semester = new Semester(makePrettyText(semesterName));
             setTitle(APP_NAME + " | " + semester.getName());
         }
     }
@@ -121,6 +163,14 @@ public class FlashMemoryGUI extends JFrame {
                 JOptionPane.showMessageDialog(this, "Unable to load semester:\n" + filepath);
             }
         }
+    }
+
+    //effects: removes white space and quotation marks around s
+    // adapted from FitLifeGymKiosk @ https://github.com/UBCx-Software-Construction/long-form-problem-starters.git
+    private String makePrettyText(String s) {
+        s = s.trim();
+        s = s.replaceAll("\"|\'", "");
+        return s;
     }
 
     public static void main(String[] args) {
